029bb90b6fd91de471c2326057e5f5ee05b6813f
oladraxler01 <olasukanmitayos@gmail.com>
2026-07-27 01:23:56 +0100

    Use gallery photos for memory match

--- src/components/GameZone.tsx
+++ src/components/GameZone.tsx
@@ -62,9 +62,9 @@ const memoryIcons = [
   "/images/matching_game/SAVE_20260704_202817.jpg",
   "/images/matching_game/IMG_9412.jpg",
   "/images/matching_game/IMG_9510.jpg",
-  "💍",
-  "💐",
-  "🎂",
+  "/images/IMG_0355.jpg",
+  "/images/soul_connection.jpg",
+  "/images/The Silly side of love.jpg",
 ];
 
 function shuffleArray<T>(arr: T[]): T[] {
