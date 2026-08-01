a78e80e4794478a5fc9dc7d1413c578e9da288ff
oladraxler01 <olasukanmitayos@gmail.com>
2026-08-01 01:54:18 +0100

    Replace synthetic SVG pocket with photorealistic generated image

--- src/components/EnvelopeLoader.tsx
+++ src/components/EnvelopeLoader.tsx
@@ -163,55 +163,17 @@ export default function EnvelopeLoader() {
                 </p>
               </motion.div>
 
-              {/* Front Laser-Cut Red Filigree Pocket */}
-              <div className="absolute bottom-0 left-0 right-0 h-[65%] sm:h-[70%] z-20 pointer-events-none drop-shadow-[0_-8px_15px_rgba(0,0,0,0.4)]">
-                <svg className="w-full h-full" viewBox="0 0 1000 600" preserveAspectRatio="none">
-                  <defs>
-                    <linearGradient id="pocketGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                      <stop offset="0%" stopColor="#D2143A" />
-                      <stop offset="100%" stopColor="#8A0A1F" />
-                    </linearGradient>
-
-                    {/* Highly ornate single swirl motif to be layered and repeated */}
-                    <g id="ornate-swirl" stroke="url(#pocketGradient)" strokeLinecap="round" fill="none">
-                      <path d="M0,0 C 50,-50 100,-10 120,40 C 140,90 90,130 50,100 C 10,70 30,10 80,20" strokeWidth="12" />
-                      <path d="M120,40 C 160,-10 220,10 200,70 C 180,130 130,100 150,60" strokeWidth="10" />
-                      <path d="M50,100 C 20,150 80,200 120,160 C 160,120 120,80 90,110" strokeWidth="8" />
-                      <path d="M 80,20 Q 60,-20 90,-40 Q 110,-10 80,20" fill="url(#pocketGradient)" strokeWidth="2" />
-                      <path d="M 200,70 Q 250,50 270,80 Q 230,110 200,70" fill="url(#pocketGradient)" strokeWidth="2" />
-                      <circle cx="90" cy="110" r="12" fill="url(#pocketGradient)" />
-                      <circle cx="150" cy="60" r="8" fill="url(#pocketGradient)" />
-                    </g>
-                  </defs>
-
-                  {/* Solid Bottom Base forming the structural pocket */}
-                  <path d="M 0,600 L 1000,600 L 1000,500 Q 750,380 500,550 Q 250,380 0,500 Z" fill="url(#pocketGradient)" />
-
-                  {/* Dense layered swirls clustering up the left corner */}
-                  <use href="#ornate-swirl" x="0" y="200" transform="scale(1.5) rotate(-30 0 200)" />
-                  <use href="#ornate-swirl" x="-50" y="380" transform="scale(1.8) rotate(-10 -50 380)" />
-                  <use href="#ornate-swirl" x="150" y="450" transform="scale(1.2) rotate(45 150 450)" />
-                  <use href="#ornate-swirl" x="300" y="500" transform="scale(0.8) rotate(70 300 500)" />
-                  <use href="#ornate-swirl" x="220" y="280" transform="scale(0.9) rotate(15 220 280)" />
-                  <use href="#ornate-swirl" x="80" y="100" transform="scale(1) rotate(-45 80 100)" />
-
-                  {/* Dense layered swirls clustering up the right corner (mirrored) */}
-                  <g transform="translate(1000, 0) scale(-1, 1)">
-                    <use href="#ornate-swirl" x="0" y="200" transform="scale(1.5) rotate(-30 0 200)" />
-                    <use href="#ornate-swirl" x="-50" y="380" transform="scale(1.8) rotate(-10 -50 380)" />
-                    <use href="#ornate-swirl" x="150" y="450" transform="scale(1.2) rotate(45 150 450)" />
-                    <use href="#ornate-swirl" x="300" y="500" transform="scale(0.8) rotate(70 300 500)" />
-                    <use href="#ornate-swirl" x="220" y="280" transform="scale(0.9) rotate(15 220 280)" />
-                    <use href="#ornate-swirl" x="80" y="100" transform="scale(1) rotate(-45 80 100)" />
-                  </g>
-
-                  {/* Sweeping connection lines forming the classic laser-cut framework */}
-                  <path d="M 0,350 C 150,450 300,500 500,550 C 700,500 850,450 1000,350" fill="none" stroke="url(#pocketGradient)" strokeWidth="18" strokeLinecap="round" />
-                  <path d="M 0,200 C 200,320 350,480 500,550 C 650,480 800,320 1000,200" fill="none" stroke="url(#pocketGradient)" strokeWidth="10" strokeLinecap="round" />
-                  <path d="M 0,50 C 250,250 400,450 500,550 C 600,450 750,250 1000,50" fill="none" stroke="url(#pocketGradient)" strokeWidth="5" strokeLinecap="round" />
-                  <path d="M 0,280 C 180,380 320,460 500,550 C 680,460 820,380 1000,280" fill="none" stroke="url(#pocketGradient)" strokeWidth="8" strokeLinecap="round" />
-                </svg>
-              </div>
+              {/* Front Photorealistic Laser-Cut Red Filigree Pocket */}
+              <div 
+                className="absolute bottom-0 left-0 right-0 h-[70%] sm:h-[80%] z-20 pointer-events-none transition-opacity duration-1000"
+                style={{
+                  backgroundImage: `url('/images/laser-pocket.png')`,
+                  backgroundSize: '100% 100%',
+                  backgroundPosition: 'bottom',
+                  backgroundRepeat: 'no-repeat',
+                  mixBlendMode: 'multiply'
+                }}
+              ></div>
               </div>
             </motion.div>
         )}
